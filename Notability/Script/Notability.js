0a7d90df4b771525d085b9198e61ff7661de9fb9
--globalnihaotang <--globalsmilingdon@163.com>
2024-12-17 22:37:16 +0800

    nitability-Unlock

---
+++ Notability/Script/Notability.js
@@ -0,0 +1,28 @@
+var tanhax = JSON.parse($response.body);
+
+tanhax = {
+   "data" : {
+     "processAppleReceipt" : {
+       "error" : 0,
+       "subscription" : {
+         "productId" : "com.gingerlabs.Notability.premium_subscription",
+         "originalTransactionId" : "710000869822929",
+         "tier" : "premium",
+         "refundedDate" : null,
+         "refundedReason" : null,
+         "isInBillingRetryPeriod" : false,
+         "expirationDate" : "1314-02-27T22:16:52.000Z",
+         "gracePeriodExpiresAt" : null,
+         "overDeviceLimit" : false,
+         "expirationIntent" : null,
+         "__typename" : "AppStoreSubscription",
+         "user" : null,
+         "status" : "canceled",
+         "originalPurchaseDate" : "2021-02-27T22:16:52.000Z"
+       },
+       "__typename" : "SubscriptionResult"
+    }
+  }
+};
+
+$done({body : JSON.stringify(tanhax)});
